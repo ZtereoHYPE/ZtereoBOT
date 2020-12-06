10453cae4cff95e02151cd7fb81525479a095cf4
ZtereoHYPE <57519662+ZtereoHYPE@users.noreply.github.com>
2020-12-06 01:54:11 +0100

    new: new help check for -help no matter prefix

--- commands/help.js
+++ commands/help.js
@@ -4,12 +4,14 @@ const database = require('../database.json');
 module.exports = {
 	name: 'help',
 	description: 'List all of my commands or info about a specific command.',
-	execute(message, args) {
+	execute(message) {
         const { commands } = message.client;
 		const embed = new Discord.MessageEmbed()
             .setColor('#00cc00')
 			.setTitle('Commmand List:')
+			.setDescription(`My prefix here is \`${database[`${message.guild.id}`]["prefix"]}\``)
 			.setFooter(`PS: to have more information about a command, use the command with no arguments.`)
+			.setTimestamp()
 		let i;
 		for (i = 0; i < commands.map(command => command.name).length; i++) {
 			embed.addField(`${database[`${message.guild.id}`]["prefix"]}${commands.map(command => command.name)[i]}`, commands.map(command => command.description)[i]);
