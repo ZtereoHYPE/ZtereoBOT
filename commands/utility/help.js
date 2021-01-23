54ded12dfee1e84577441a6db84463022a262c3c
ZtereoHYPE <57519662+ZtereoHYPE@users.noreply.github.com>
2021-01-24 00:40:02 +0100

    new: help self-destruct after 20s

--- commands/utility/help.js
+++ commands/utility/help.js
@@ -9,11 +9,11 @@ module.exports = {
 			.setColor('#8EB9FE')
 			.setAuthor('Command List:', 'https://i.imgur.com/dSTYnIF.png')
 			.setDescription(`My prefix here is \`${database[`${message.guild.id}`]["prefix"]}\``)
-			.setFooter(`PS: to have more information about a command, use the command with no arguments.`)
+			.setFooter(`This message will self-destruct in 20s`)
 		let i;
 		for (i = 0; i < commands.map(command => command.name).length; i++) {
 			embed.addField(`${database[`${message.guild.id}`]["prefix"]}${commands.map(command => command.name)[i]}`, commands.map(command => command.description)[i]);
 		};
-		message.channel.send(embed);
+		message.channel.send(embed).then(newMessage => newMessage.delete({timeout: 20000}))
 	},
 };
