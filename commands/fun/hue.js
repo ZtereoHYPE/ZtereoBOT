dd8c64fad5b1ebbf60cade53f13a0c1517fab021
ZtereoHYPE <>
2021-01-24 23:25:44 +0100

    change: allow only ztereo to control his hue light

--- commands/fun/hue.js
+++ commands/fun/hue.js
@@ -5,6 +5,12 @@ module.exports = {
     category: path.dirname(__filename).split(path.sep).pop(),
 	description: 'a commnand used to control ZtereoHYPE\'s hue light bulbs',
 	execute(message, args, client, database) {
+
+        if (message.guild.member(message.author).id != 434842825805266944) {
+            message.reply("Bruh only ztereohype can control his hue lights.")
+            return;
+        }
+
         const hue = new Hue('ecb5fafffe1a9fac', '-Louw4QvRiDCeB2wSjVDV2sQXGfqZgQphVb35ZYt');
 
         hue.on('ready', async (bridge) => {
