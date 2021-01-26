29810ed09d33e1ff7b91331d2e3231aaf926ba60
user.name <lorenzodegrandis@icloud.com>
2021-01-26 23:05:57 +0100

    new: database converter! preparing for v2 merge!

--- commands/bot/convertDatabase.js
+++ commands/bot/convertDatabase.js
@@ -2,17 +2,29 @@ const fs = require('fs');
 const path = require('path')
 const Discord = require('discord.js');
 module.exports = {
-    name: 'name',
+    name: 'convertdatabase',
     category: path.dirname(__filename).split(path.sep).pop(),
-	description: 'description',
+	description: 'convert the Database',
 	execute(message, args, client, database, shortcuts) {
 
+        message.channel.send("i've started converting the database")
         // save each key of the json in a variable
+        Object.keys(database).forEach(key => {
+            delete database[key]["bans"];
 
-        /* iterate over each key:
+            /* iterate over each key:
             - delete bans
-            - if enabled has tts, delete enabled and create empty disabled
-            - if enabled does not have tts (is empty), create disabled containing tts */
-        
+            - if enable has tts, delete enabled and create empty disabled
+            - if enable does not have tts (is empty), create disabled containing tts */
+            if (database[key]["enable"].includes("tts")) {
+                delete database[key]["enable"];
+                database[key]["disabled"] = [];
+            } else {
+                delete database[key]["enable"];
+                database[key]["disabled"] = ["tts"];
+            }
+        });
+        shortcuts.functions.saveDatabase(database)
+        message.channel.send("i've tried converting the database")
 	},
 };
