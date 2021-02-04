295f080902bdd90cada3e44d8235f6c9a3326f0c
user.name <57519662+ZtereoHYPE@users.noreply.github.com>
2021-02-04 23:27:42 +0100

    new: xkcd goodness

--- commands/fun/xkcd.js
+++ commands/fun/xkcd.js
@@ -6,10 +6,8 @@ module.exports = {
     category: path.dirname(__filename).split(path.sep).pop(),
     description: 'Only the best comics.',
     execute(message) {
-        let xkcdAPI = {
-            link: 'https://xkcd.com',
-        }
-        https.get(xkcd.link, (resp) => {
+        // gather what the last XKCD comic is
+        https.get('https://xkcd.com/info.0.json', (resp) => {
             let data = '';
 
             resp.on('data', (chunk) => {
@@ -17,23 +15,33 @@ module.exports = {
             });
 
             resp.on('end', () => {
-                const embed = new MessageEmbed()
-                    .setColor('#' + (Math.random() * 0xFFFFFF << 0).toString(16))
-                    .setTitle(JSON.parse(data)["safe_title"])
-                    .setDescription(JSON.parse(data)['img'])
-                    .setFooter(JSON.parse(data)['month'] + "/" + JSON.parse(data)['day'] + "/" + JSON.parse(data)['year'])
-                    .setTimestamp()
-                message.channel.send(embed);
-            });
-            
-        }).on("error", (err) => {
-            const embed = new MessageEmbed()
-                .setColor('red')
-                .setTitle('Oopsie woopsie!')
-                .setDescription('UwU the bot did a fucky wucky! A little fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!')
-                .setFooter('kill me please')
-                .setTimestamp()
-            message.channel.send(embed);
-        });
+                https.get(`https://xkcd.com/${Math.floor(Math.random() * JSON.parse(data)["num"])}/info.0.json`, (resp) => {
+                    let data = '';
+        
+                    resp.on('data', (chunk) => {
+                        data += chunk;
+                    });
+        
+                    resp.on('end', () => {
+                        const embed = new MessageEmbed()
+                            .setColor('#' + (Math.random() * 0xFFFFFF << 0).toString(16))
+                            .setTitle(JSON.parse(data)["title"])
+                            .setImage(JSON.parse(data)['img'])
+                            .setFooter(JSON.parse(data)['month'] + "/" + JSON.parse(data)['day'] + "/" + JSON.parse(data)['year'])
+                            .setTimestamp()
+                        message.channel.send(embed);
+                    });
+        
+                }).on("error", (err) => {
+                    const embed = new MessageEmbed()
+                        .setColor('FF1B1B')
+                        .setTitle('Oopsie woopsie!')
+                        .setDescription('UwU the bot did a fucky wucky! A little fucko boingo! The code monkeys at our headquarters are working VEWY HAWD to fix this!')
+                        .setFooter('kill me please')
+                        .setTimestamp()
+                    message.channel.send(embed);
+                });
+            })
+        })
     }
 }
