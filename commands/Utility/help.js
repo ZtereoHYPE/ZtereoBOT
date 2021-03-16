1e61d984ca79bccf35d122e6953ce471d8e70cb1
ZtereoHYPE <lorenzodegrandis@icloud.com>
2021-03-16 11:46:20 +0100

    new: refactored help command to use DMs

--- commands/Utility/help.js
+++ commands/Utility/help.js
@@ -4,7 +4,7 @@ module.exports = {
 	name: 'help',
 	category: path.dirname(__filename).split(path.sep).pop(),
 	description: 'List all of my commands or info about a specific command.',
-	execute(message, args, database) {
+	execute(message, args, database, shortcuts) {
 		// let commands be the client's commnads
 		const { commands } = message.client;
 
@@ -14,9 +14,9 @@ module.exports = {
 		for (var i = 0; i < categoryArray.length; ++i) {
 			embedArray[i] = new MessageEmbed()
 				.setColor('#8EB9FE')
-				.setAuthor(categoryArray[i] + ':', 'https://i.imgur.com/dSTYnIF.png')
+				.setAuthor(categoryArray[i], 'https://i.imgur.com/dSTYnIF.png')
 			for (correctCommand of commandsArray.filter(value => value.category == categoryArray[i])) {
-				embedArray[i].addField(`${database[`${message.guild.id}`]["prefix"]}${correctCommand.name}`, correctCommand.description)
+				embedArray[i].addField(`${database[`${message.guild.id}`]["prefix"]}${correctCommand.name + ':'}`, correctCommand.description)
 			}
 		}
 
